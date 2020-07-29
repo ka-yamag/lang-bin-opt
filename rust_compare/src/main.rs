3f6d4ce436c77d4a58566cddafc6fdd84c92245b
katyamag <pupupu0701@gmail.com>
2020-07-29 18:38:38 +0900

    impl rust compare

--- rust_compare/src/main.rs
+++ rust_compare/src/main.rs
@@ -1,24 +1,34 @@
 fn main() {
-    let a = "abcdefghijklkm012345";
-    let b = "012345:;++)))(*(&*^%kadjfklajfklaikadjfkladjfkladjsj";
+    let a: &'static str = "abcdefghijklkm012345";
+    let b: &'static str = "012345:;++)))(*(&*^%kadjfklajfklaikadjfkladjfkladjsj";
 
-    if constantTimeCompare(a.as_bytes(), b.as_bytes()) {
+    if constant_time_compare(a.as_bytes(), b.as_bytes()) {
         println!("match!");
     } else {
         println!("unmatch!");
     }
 }
 
-fn constantTimeCompare(x: &[u8], y: &[u8]) -> bool {
+fn constant_time_compare(x: &[u8], y: &[u8]) -> bool {
     if x.len() != y.len() {
-        false
+        return false
     }
 
-    let v: [u8; x.len()];
+    let mut _v: u8 = 0x00;
 
     for (i, _) in x.iter().enumerate() {
-        v |= x[i] ^ y[i]
+        _v |= x[i] ^ y[i];
+    }
+
+    if constant_time_byteeq(_v, 0) == 0 {
+        return false
     }
 
     true
+}
+
+fn constant_time_byteeq(x: u8, y: u8) -> i32 {
+    let binary_xor: i32 = From::from(x ^ y);
+
+    (binary_xor - 1) >> 31
 }
